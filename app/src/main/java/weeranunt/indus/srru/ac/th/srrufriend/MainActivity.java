8b64f63915d91b22a8b832673625434870008a67
AjWweeranunt <weeranunv@gmail.com>
2018-06-27 13:13:02 +0700

    Initial commit

---
+++ app/src/main/java/weeranunt/indus/srru/ac/th/srrufriend/MainActivity.java
@@ -0,0 +1,13 @@
+package weeranunt.indus.srru.ac.th.srrufriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
